321b70ae0914a7e3d13b4cea363cd18c122ae138
Charuvahan Adhivarahan <31253536+charuvah@users.noreply.github.com>
2024-06-19 23:05:51 -0400

    Metalbones add types and functions

--- colors/metalbones.lua
+++ colors/metalbones.lua
@@ -12,7 +12,16 @@ if bg == "light" then
 else
 	palette = util.palette_extend({
 		khold = hsluv "#974b46",
-		nile = hsluv "#777755",
+		metalred = hsluv "#835d5a",
+		metalgreen = hsluv "#628282",
+		metalblue = hsluv "#5f81a5",
+		metalgray1 = hsluv "#121212",
+		metalgray2 = hsluv "#222222",
+		metalgray3 = hsluv "#4f4f4f",
+		metalgray4 = hsluv "#5f5f5f",
+		metalgray5 = hsluv "#919191",
+		metalgray6 = hsluv "#a1a1a1",
+		metalgray7 = hsluv "#c1c1c1",
 	}, bg)
 end
 
@@ -21,7 +30,16 @@ local base_specs = generator.generate(palette, bg, generator.get_global_config(c
 
 local specs = lush.extends({ base_specs }).with(function()
 	return {
-		String { base_specs.String, fg = palette.nile },
+		Normal { base_specs.Normal, fg = palette.metalgray5 },
+		Number { base_specs.Number, fg = palette.khold },
+		Float { base_specs.Float, fg = palette.khold },
+		Constant { base_specs.Constant, fg = palette.metalgreen },
+		Statement { base_specs.Statement, fg = palette.metalgray6 },
+		Type { base_specs.Type, fg = palette.metalgray3 },
+		Function { base_specs.Function, fg = palette.metalgray4 },
+		GitSignsAdd { base_specs.GitSignsAdd, fg = palette.metalgreen },
+		GitSignsDelete { base_specs.GitSignsDelete, fg = palette.metalred },
+		GitSignsChange { base_specs.GitSignsDelete, fg = palette.metalblue },
 	}
 end)
 
