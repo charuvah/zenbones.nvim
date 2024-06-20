82a851b34d2206625ee9cc95cd439eb4d8a14958
Charuvahan Adhivarahan <31253536+charuvah@users.noreply.github.com>
2024-06-19 22:02:08 -0400

    Add first draft of metalbones

---
+++ colors/metalbones.lua
@@ -0,0 +1,28 @@
+local colors_name = "metalbones"
+vim.g.colors_name = colors_name
+
+local lush = require "lush"
+local hsluv = lush.hsluv
+local util = require "zenbones.util"
+
+local bg = vim.o.background
+
+local palette
+if bg == "light" then
+else
+	palette = util.palette_extend({
+		khold = hsluv "#974b46",
+		nile = hsluv "#777755",
+	}, bg)
+end
+
+local generator = require "zenbones.specs"
+local base_specs = generator.generate(palette, bg, generator.get_global_config(colors_name, bg))
+
+local specs = lush.extends({ base_specs }).with(function()
+	return {
+		String = nile,
+	}
+end)
+
+lush(specs)
